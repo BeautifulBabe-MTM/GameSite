160c5bdf2c40e91126513b87c915878bd676cf2e
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2023-01-25 23:35:46 +0200

    adding game-add

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,21 +2,22 @@ import React from "react";
 
 var login, password;
 export default function Navbar() {
-    return ( 
+    return (
         <nav className="nav">
-        <div className="nav-li"><a href="/">Главная</a></div>
-        <div className="nav-li" onClick={RouteToSomewhere}>Контакты</div>
-        <div className="nav-li" onClick={SignIn}>Кабинет</div>
+            <div className="nav-li"><a href="/">Главная</a></div>
+            <div className="nav-li" onClick={Contacts}>Контакты</div>
+            <div className="nav-li" onClick={SignIn}>Кабинет</div>
+            <div className="nav-li">Добавить игру (Admin)</div>
         </nav>
     );
 }
 
-function RouteToSomewhere(){
+function Contacts() {
     alert("Наши контакты:\n+380 44 500 70 40 - Ярослав\n+380 44 232 69 78 - Руслан\nНаши инстаграммы указаны в самом низу страницы! Пока");
 }
-function SignIn(){
+function SignIn() {
     login = prompt("Введите логин");
     password = prompt("Введите пароль");
     console.log(login);
-console.log(password);
+    console.log(password);
 }
