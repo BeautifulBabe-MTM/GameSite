7ae4494267dccd8ba8f8cfaaa860f9123dd08313
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2023-01-25 22:55:12 +0200

    added connected database

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require ('express');
+const app = express();
+const mongoose = require('mongoose');
+const GameModel = require('./models/GameModel');
+
+
+const URI ="mongodb+srv://admin:123zxc34@cluster0.hoxv5bc.mongodb.net/?retryWrites=true&w=majority";
+
+async function run() {
+  try {
+    await mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    mongoose.set('strictQuery', true);
+    app.listen(3001, () => {
+        console.log(`Server is working now on port ${3001}`);
+    });
+}
+catch (e) {
+    console.log(e.message);
+}
+}
+
+run();
+
+app.get("/getgames", async (req, res) => {
+    const cursor = await GameModel.find({}).lean();
+  res.json({cursor:cursor})
+})
+
+app.get('/addgame',async(req,res)=>{
+    let Name = "Grand Theft Auto: Vice City";
+    let Desc = "Simple text example";
+    let Price = "8.99";
+    let Img = "https://w0.peakpx.com/wallpaper/780/350/HD-wallpaper-gta-vice-city-games.jpg";
+    
+
+    const game = new GameModel({
+        Name:Name,
+        Desc:Desc,
+        Price:Price,
+        Img:Img
+    })
+    await game.save();
+})  
